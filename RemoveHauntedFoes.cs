e119c98096fcb300039eab38659db8318847a017
CharlesGameDev <99226468+CharlesGameDev@users.noreply.github.com>
2023-04-16 17:11:57 -0400

    1.0.0.1
    Fixed issues with music and added settings

--- RemoveHauntedFoes.cs
+++ RemoveHauntedFoes.cs
@@ -1,15 +1,11 @@
-﻿using System;
+﻿using Modding;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using GlobalEnums;
-using IL.InControl.UnityDeviceProfiles;
-using Modding;
-using Modding.Delegates;
 using UnityEngine;
 using WavLib;
-using Random = UnityEngine.Random;
 
 namespace RemoveHauntedFoes
 {
@@ -22,16 +18,15 @@ namespace RemoveHauntedFoes
         public static Dictionary<string, AudioClip> soundDict;
         public static Dictionary<string, Texture2D> imageDict;
         string currentScene;
+        bool NKG = true;
+        bool NOSK = true;
 
         new public string GetName() => "Remove Haunted Foes";
-        public override string GetVersion() => "v1.0.0";
+        public override string GetVersion() => "v1.0.0.1";
         public override void Initialize()
         {
-            Log("Loading resources");
             dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             musicDir = dir + "\\Resources\\Music";
-            DebugLog("Resources folder: " + dir);
-            DebugLog("Resources/Music folder: " + musicDir);
             musicDict = new Dictionary<string, AudioClip>();
             foreach (string file in Directory.GetFiles(musicDir))
             {
@@ -40,48 +35,88 @@ namespace RemoveHauntedFoes
             }
 
             ModHooks.BeforeSceneLoadHook += BeforeSceneLoad;
-            On.HeroController.Update += HeroUpdate;
-            On.AudioManager.ApplyMusicCue += ApplyMusicCue;
-        }
-
-        void ApplyMusicCue(On.AudioManager.orig_ApplyMusicCue orig, AudioManager self, MusicCue musicCue, float delayTime, float transitionTime, bool applySnapshot)
-        {
-            DebugLog("Music cue: " + musicCue.name);
-            if ((musicCue.name == "GG Sad" || musicCue.name == "NightmareGrimm") && currentScene == "GG_Grimm_Nightmare")
-            {
-                PlayAudioClip(musicDict["nkg"], HeroController.instance.transform.position, 0.8f);
-                return;
-            }
-            if ((musicCue.name == "GG Sad" || musicCue.name == "Grimm") && currentScene == "GG_Grimm")
-            {
-                PlayAudioClip(musicDict["nkg"], HeroController.instance.transform.position, 0.8f);
-                return;
-            }
-            if ((musicCue.name == "GG Sad" || musicCue.name == "MimicSpider") && currentScene == "GG_Nosk_Hornet")
-            {
-                PlayAudioClip(musicDict["nosk"], HeroController.instance.transform.position, 0.8f);
-                return;
-            }
-            orig(self, musicCue, delayTime, transitionTime, applySnapshot);
-        }
-
-        public void HeroUpdate(On.HeroController.orig_Update orig, HeroController self)
-        {
-            orig(self);
+            On.AudioManager.BeginApplyMusicCue += BeginApplyMusicCue;
         }
 
         public List<IMenuMod.MenuEntry> GetMenuData(IMenuMod.MenuEntry? toggleButtonEntry)
         {
             return new List<IMenuMod.MenuEntry>
             {
+                new IMenuMod.MenuEntry {
+                    Name = "Nightmare King Grimm",
+                    Description = "Use new NKG music",
+                    Values = new string[] {
+                        "Off",
+                        "On"
+                    },
+                    Saver = opt => NKG = opt switch {
+                        0 => false,
+                        1 => true,
+                        _ => throw new InvalidOperationException()
+                    },
+                    Loader = () => NKG switch {
+                        false => 0,
+                        true => 1,
+                    }
+                },
+                new IMenuMod.MenuEntry {
+                    Name = "Nosk",
+                    Description = "Use new Nosk music",
+                    Values = new string[] {
+                        "Off",
+                        "On"
+                    },
+                    Saver = opt => NOSK = opt switch {
+                        0 => false,
+                        1 => true,
+                        _ => throw new InvalidOperationException()
+                    },
+                    Loader = () => NOSK switch {
+                        false => 0,
+                        true => 1,
+                    }
+                },
             };
+        }
+
+        IEnumerator BeginApplyMusicCue(On.AudioManager.orig_BeginApplyMusicCue orig, AudioManager self, MusicCue musicCue, float delayTime, float transitionTime, bool applySnapshot)
+        {
+            //DebugLog("Music cue: " + musicCue.name);
+
+            MusicCue.MusicChannelInfo[] infos = ReflectionHelper.GetField<MusicCue, MusicCue.MusicChannelInfo[]>(musicCue, "channelInfos");
+            bool changed = false;
+
+            foreach (MusicCue.MusicChannelInfo info in infos)
+            {
+                AudioClip possibleReplace = GetAudioClip(musicCue.name);
+                if (possibleReplace != null)
+                {
+                    ReflectionHelper.SetField(info, "clip", possibleReplace);
+                    changed = true;
+                }
+            }
+
+            if (changed)
+                ReflectionHelper.SetField(musicCue, "channelInfos", infos);
+
+            yield return orig(self, musicCue, delayTime, transitionTime, applySnapshot);
+        }
+
+        public AudioClip GetAudioClip(string name)
+        {
+            if (NKG && ((name == "GG Sad" || name == "NightmareGrimm") && currentScene == "GG_Grimm_Nightmare")) return musicDict["nkg"];
+            //if (GRIMM && ((name == "GG Sad" || name == "Grimm") && currentScene == "GG_Grimm")) return musicDict["grimm"];
+            if (NOSK && ((name == "GG Sad" || name == "MimicSpider") && (currentScene == "GG_Nosk_Hornet" || currentScene == "GG_Nosk"))) return musicDict["nosk"];
+
+
+            return null;
         }
 
         private AudioClip GetAudioClip(string dir, string origName)
         {
             if (File.Exists($"{dir}/{origName}.wav"))
             {
-                DebugLog($"Using audio file \"{origName}.wav\"");
+                //DebugLog($"Using audio file \"{origName}.wav\"");
                 FileStream stream = File.OpenRead($"{dir}/{origName}.wav");
                 WavData.Inspect(stream, null);
                 WavData wavData = new();
@@ -100,25 +135,12 @@ namespace RemoveHauntedFoes
                 float[] wavSoundData = wavData.GetSamples();
                 AudioClip audioClip = AudioClip.Create(origName, wavSoundData.Length / wavData.FormatChunk.NumChannels, wavData.FormatChunk.NumChannels, (int)wavData.FormatChunk.SampleRate, false);
                 audioClip.SetData(wavSoundData, 0);
-                DebugLog($"Loaded \"{origName}.wav\"");
+                //DebugLog($"Loaded \"{origName}.wav\"");
                 return audioClip;
-                //return WavUtility.ToAudioClip($"{_dir}/{origName}.wav");
             }
 
-            DebugLog($"File not found for \"{origName}\"");
+            //DebugLog($"File not found for \"{origName}\"");
             return null;
-        }
-
-        public void PlayAudioClip(AudioClip clip, Vector3 pos, float volume = 1)
-        {
-            DebugLog("PLAYING AUDIO CLIP: " + clip.name + " AT " + pos.ToString() + " WITH VOLUME " + volume);
-            GameObject o = new(clip.name);
-            o.transform.position = pos;
-            AudioSource source = o.AddComponent<AudioSource>();
-            source.volume = volume;
-            source.loop = true;
-            source.clip = clip;
-            source.Play();
         }
 
         public string BeforeSceneLoad(string newSceneName)
@@ -128,10 +150,12 @@ namespace RemoveHauntedFoes
             return newSceneName;
         }
 
+        /*
         public static void DebugLog(string msg)
         {
             Modding.Logger.Log(msg);
-            Debug.Log("[KnightsBizarreAdevnture] - " + msg);
+            Debug.Log("[RemoveHauntedFoes] - " + msg);
         }
+        */
     }
 }
